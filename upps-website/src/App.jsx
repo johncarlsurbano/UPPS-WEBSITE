cb099b8feec95d45dd7294dd8965de8e886d5c65
johncarlsurbano <surbano.johncarll@gmail.com>
2024-09-09 23:50:16 +0800

    completed Header1

--- upps-website/src/App.jsx
+++ upps-website/src/App.jsx
@@ -1,11 +1,10 @@
-import Button from '@mui/material/Button';
 import '../src/styles/fonts.css'
+import { Header1 } from './components/Header1'
 
 function App() {
   return (
     <>
-    <h2 className='text-3xl text-uppsyellow font-inter18 '>hello</h2>
-    <h2 className='text-3xl text-uppsyellow '>hello</h2>
+    <Header1></Header1>
     </>
   )
 }
--- upps-website/src/components/Header1.jsx
+++ upps-website/src/components/Header1.jsx
@@ -1,23 +1,28 @@
-import '../styles/Header1.css';
+import "../styles/Header1.css";
+import logo from "../assets/upps-website-logo.png"
 
-export const Header1 = () =>{
-    return(
-        <div className="header1">
-            <div className="navbar1">
-                <div className="navbar1-content"></div>
-            </div>
-            <div className="navbar2">
-                <div className="navbar2-content">
-                    <div className="navbar2-content-upps-logo">
-                        <img src="" alt="" />
-                    </div>
-                    <div className="navbar2-content-links">
-                        <a href="">Home</a>
-                        <a href="">About Us</a>
-                        <a href="">Login</a>
-                    </div>
-                </div>
-            </div>
+export const Header1 = () => {
+  return (
+    <div className="header">
+      <div className="navbar1 bg-uppsyellow">
+        <div className="navbar1-content bg-uppsdarkblue"></div>
+      </div>
+      <div className="navbar2">
+        <div className="navbar2-content">
+          <div className="navbar2-content-logo">
+            <img src={logo} alt="" />
+          </div>
+          <div className="navbar2-content-links1">
+            <a href="">Home</a>
+            <a href="">About Us</a>
+            <a href="">Services</a>
+          </div>
+          <div className="navbar2-content-links2">
+            <a className="bg-uppsyellow text-white" id="navbar2-button" href="">Dashboard</a>
+            <a id="navbar2-button-login" href="">Login</a>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
