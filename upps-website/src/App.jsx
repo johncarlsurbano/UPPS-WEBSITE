470da2dda8c5585fef5bc2cd3933ad1624013791
brixdaomilas <daomilas.brixd@gmail.com>
2024-09-09 23:00:19 +0800

    Add and Set the default font

--- upps-website/src/App.jsx
+++ upps-website/src/App.jsx
@@ -1,9 +1,11 @@
 
 import Button from '@mui/material/Button';
+import '../src/styles/fonts.css'
+
 function App() {
   return (
     <>
-    <h2 className='text-3xl text-uppsyellow '>hello</h2>
+    <h2 className='text-3xl text-uppsyellow font-inter18 '>hello</h2>
     </>
   )
 }
